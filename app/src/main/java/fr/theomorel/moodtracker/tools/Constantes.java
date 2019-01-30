21fe45445d012ee2ca042fbafe5da6f575ed926e
Théo Morel <morel.theo@wanadoo.fr>
2019-01-30 19:33:31 +0100

    Ajout des Tools

---
+++ app/src/main/java/fr/theomorel/moodtracker/tools/Constantes.java
@@ -0,0 +1,7 @@
+package fr.theomorel.moodtracker.tools;
+
+public class Constantes {
+    public static String keyMood = "keyMoodDay";
+    public static String keyTableau = "moodTracker";
+    public static String keyHistory = "historyList";
+}
